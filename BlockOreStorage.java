1fad34809b9bc0047c706f21d385529d0e4cf47b
Danice123 <Mitchelldanice@gmail.com>
2012-03-25 01:16:53 -0400

    version 1.1 - no images!

--- BlockOreStorage.java
+++ BlockOreStorage.java
@@ -7,20 +7,56 @@ public class BlockOreStorage extends Block
         super(par1, Material.iron);
         blockIndexInTexture = par2;
     }
-
+    
+    public int getRenderColor(int par1)
+    {
+        if (par1 == 1){return 0xff9900;} //orange
+        if (par1 == 2){return 0x990099;} //magenta
+        if (par1 == 3){return 0x33ffff;} //light blue
+        if (par1 == 4){return 0xffff00;} //yellow
+        if (par1 == 5){return 0x33ff00;} //lime
+        if (par1 == 6){return 0xff00ff;} //pink
+        if (par1 == 7){return 0x999999;} //grey
+        if (par1 == 8){return 0xcccccc;} //light grey
+        if (par1 == 9){return 0x33cccc;} //cyan
+        if (par1 == 10){return 0x660066;} //purple
+        if (par1 == 11){return 0x0000ff;} //blue
+        if (par1 == 12){return 0x996600;} //brown
+        if (par1 == 13){return 0x006600;} //green
+        if (par1 == 14){return 0xff0000;} //red
+        if (par1 == 15){return 0x666666;} //black
+        if (par1 == 16){return 0x990000;} //dark red
+        return 0xffffff;
+    }
+    
+    public int colorMultiplier(IBlockAccess par1IBlockAccess, int par2, int par3, int par4)
+    {
+    	int l = par1IBlockAccess.getBlockMetadata(par2, par3, par4);
+        if (l == 1){return 0xff9900;} //orange
+        if (l == 2){return 0x990099;} //magenta
+        if (l == 3){return 0x33ffff;} //light blue
+        if (l == 4){return 0xffff00;} //yellow
+        if (l == 5){return 0x33ff00;} //lime
+        if (l == 6){return 0xff00ff;} //pink
+        if (l == 7){return 0x999999;} //grey
+        if (l == 8){return 0xcccccc;} //light grey
+        if (l == 9){return 0x33cccc;} //cyan
+        if (l == 10){return 0x660066;} //purple
+        if (l == 11){return 0x0000ff;} //blue
+        if (l == 12){return 0x996600;} //brown
+        if (l == 13){return 0x006600;} //green
+        if (l == 14){return 0xff0000;} //red
+        if (l == 15){return 0x666666;} //black
+        if (l == 16){return 0x990000;} //dark red
+        return 0xffffff;
+    }
+    
     /**
      * From the specified side and block metadata retrieves the blocks texture. Args: side, metadata
      */
     public int getBlockTextureFromSideAndMetadata(int par1, int par2)
     {
-        if (par2 == 0)
-        {
-            return blockIndexInTexture;
-        }
-        else
-        {
-            return mod_ColoredSteel.color[par2 - 1];
-        }
+    	return blockIndexInTexture;
     }
     
     /**
--- mod_ColoredSteel.java
+++ mod_ColoredSteel.java
@@ -2,27 +2,10 @@ package net.minecraft.src;
 
 public class mod_ColoredSteel extends BaseMod {
 
-	public static int color[] = new int[15];
 	public static final Item ColoredSteel = new ItemMetadata(Block.blockSteel.blockID - 256, Block.blockSteel).setItemName("Steel");
 	
 	public void load() {
-		color[0] = ModLoader.addOverride("/terrain.png", "/img/orange.png");
-		color[1] = ModLoader.addOverride("/terrain.png", "/img/magenta.png");
-		color[2] = ModLoader.addOverride("/terrain.png", "/img/lightblue.png");
-		color[3] = ModLoader.addOverride("/terrain.png", "/img/yellow.png");
-		color[4] = ModLoader.addOverride("/terrain.png", "/img/lime.png");
-		color[5] = ModLoader.addOverride("/terrain.png", "/img/pink.png");
-		color[6] = ModLoader.addOverride("/terrain.png", "/img/grey.png");
-		color[7] = ModLoader.addOverride("/terrain.png", "/img/lightgrey.png");
-		color[8] = ModLoader.addOverride("/terrain.png", "/img/cyan.png");
-		color[9] = ModLoader.addOverride("/terrain.png", "/img/purple.png");
-		color[10] = ModLoader.addOverride("/terrain.png", "/img/blue.png");
-		color[11] = ModLoader.addOverride("/terrain.png", "/img/brown.png");
-		color[12] = ModLoader.addOverride("/terrain.png", "/img/green.png");
-		color[13] = ModLoader.addOverride("/terrain.png", "/img/red.png");
-		color[14] = ModLoader.addOverride("/terrain.png", "/img/black.png");
 		Item.itemsList[Block.blockSteel.blockID] = ColoredSteel;
-		
         for (int i = 0; i < 16; i++)
         {
         	ModLoader.addShapelessRecipe(new ItemStack(Block.blockSteel, 1, BlockCloth.getDyeFromBlock(i)), new Object[]
@@ -33,6 +16,6 @@ public class mod_ColoredSteel extends BaseMod {
 	}
 	
 	public String getVersion() {
-		return "1.0";
+		return "1.1";
 	}
 }
